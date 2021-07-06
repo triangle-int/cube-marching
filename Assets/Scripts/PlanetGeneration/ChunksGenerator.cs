ad79926ff54aadbcf51dbbfc64b0445494134f1b
boris1592 <53943159+boris1592@users.noreply.github.com>
2021-07-06 17:21:54 +0300

    Revert "Update ChunksGenerator.cs"
    This reverts commit 0c140d5da219f465fc6cd12ab510b458b95569c5.

--- Assets/Scripts/PlanetGeneration/ChunksGenerator.cs
+++ Assets/Scripts/PlanetGeneration/ChunksGenerator.cs
@@ -38,8 +38,9 @@ namespace PlanetGeneration
                         var chunk = Instantiate(chunkPrefab, position, Quaternion.identity).GetComponent<Chunk>();
                         chunk.GenerateCubes(chunkSize, threshold, _chunkGenerator);
                         _chunks[x, y, z] = chunk;
-                        yield return null;
                     }
+                
+                    yield return null;
                 }
             }
         }
