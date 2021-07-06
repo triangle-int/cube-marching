cd8123f9e384a9671a66fb09d9e20113581bca97
boris1592 <53943159+boris1592@users.noreply.github.com>
2021-07-07 02:11:13 +0300

    compute shaders (needs some fixes)

--- Assets/Scripts/PlanetGeneration/Chunk.cs
+++ Assets/Scripts/PlanetGeneration/Chunk.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 namespace PlanetGeneration
 {
     [RequireComponent(typeof(LODGroup))]
-    [RequireComponent(typeof(MeshCollider))]
     public class Chunk : MonoBehaviour
     {
-        [SerializeField] private List<MeshFilter> lods;
-
+        [SerializeField] private List<MeshFilter> lodsMeshFilters;
+        [SerializeField] private ComputeShader shader;
+        
         private int _cubesNumber;
         private float _threshold;
         private IChunkGenerator _chunkGenerator;
 
         private Transform _transform;
         private LODGroup _lodGroup;
-        private MeshCollider _meshCollider;
+        
         private Vector4[,,] _cubes;
+        private List<MeshGenerator> _lodsMeshGenerators;
 
         private void Init(int chunkSize, float threshold, IChunkGenerator chunkGenerator)
         {
             _cubesNumber = chunkSize + 1;
             _threshold = threshold;
             _chunkGenerator = chunkGenerator;
-
-            _cubes = new Vector4[_cubesNumber, _cubesNumber, _cubesNumber];
+            
             _transform = transform;
             _lodGroup = GetComponent<LODGroup>();
-            _meshCollider = GetComponent<MeshCollider>();
+            
+            _cubes = new Vector4[_cubesNumber, _cubesNumber, _cubesNumber];
+            _lodsMeshGenerators = new List<MeshGenerator>();
+
+            for (var lod = 0; lod < lodsMeshFilters.Count; lod++)
+            {
+                var meshGenerator = new MeshGenerator(_cubesNumber, _threshold, lod, shader, lodsMeshFilters[lod]);
+                _lodsMeshGenerators.Add(meshGenerator);
+            }
         }
 
         public void GenerateCubes(int chunkSize, float threshold, IChunkGenerator chunkGenerator)
@@ -51,14 +59,8 @@ namespace PlanetGeneration
 
         private void UpdateMeshes()
         {
-            for (var lod = 0; lod < lods.Count; lod++)
-            {
-                var mesh = MeshGenerator.GenerateMesh(_cubesNumber, _threshold, _cubes, lod);
-                lods[lod].mesh = mesh;
-
-                if (lod == 0)
-                    _meshCollider.sharedMesh = mesh;
-            }
+            foreach (var meshGenerator in _lodsMeshGenerators)
+                meshGenerator.UpdateMesh(_cubes);
 
             _lodGroup.RecalculateBounds();
         }
--- Assets/Scripts/PlanetGeneration/MeshGenerator.cs
+++ Assets/Scripts/PlanetGeneration/MeshGenerator.cs
@@ -1,28 +1,133 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PlanetGeneration
 {
-    public static class MeshGenerator
+    public class MeshGenerator
     {
-        public static Mesh GenerateMesh(int cubesNumber, float threshold, Vector4[,,] cubes, int lod)
+        private struct Triangle
         {
-            var cubeSkip = 1 << lod;
-            var triangles = new List<Triangle>();
+            public Vector3 V1;
+            public Vector3 V2;
+            public Vector3 V3;
 
-            for (var x = 0; x < cubesNumber - cubeSkip; x += cubeSkip)
+            public Vector3 this[int i]
             {
-                for (var y = 0; y < cubesNumber - cubeSkip; y += cubeSkip)
+                get
                 {
-                    for (var z = 0; z < cubesNumber - cubeSkip; z += cubeSkip)
-                        triangles.AddRange(ProcessCube(cubes, threshold, cubeSkip, x, y, z));
+                    switch (i)
+                    {
+                        case 0: return V1;
+                        case 1: return V2;
+                        case 2: return V3;
+                        default: throw new IndexOutOfRangeException();
+                    }
+                }
+                set
+                {
+                    switch (i)
+                    {
+                        case 0: V1 = value; break;
+                        case 1: V2 = value; break;
+                        case 2: V3 = value; break;
+                        default: throw new IndexOutOfRangeException();
+                    }
+                }
+            }
+        }
+
+        private readonly int _cubesNumber;
+        private readonly float _threshold;
+        private readonly int _lod;
+        private readonly int _lodDownscale;
+        
+        private readonly ComputeShader _shader;
+        private readonly MeshFilter _meshFilter;
+
+        public MeshGenerator(int cubesNumber, float threshold, int lod, ComputeShader shader, MeshFilter meshFilter)
+        {
+            _cubesNumber = cubesNumber;
+            _threshold = threshold;
+            _lod = lod;
+            _lodDownscale = 1 << _lod;
+            
+            _shader = shader;
+            _meshFilter = meshFilter;
+        }
+
+        public void UpdateMesh(Vector4[,,] cubes)
+        {
+            var kernelIndex = _shader.FindKernel("March");
+            _shader.GetKernelThreadGroupSizes(kernelIndex, out var x, out var y, out var z);
+            
+            var totalCount = _cubesNumber * _cubesNumber * _cubesNumber;
+            var groupsCount = new Vector3Int(_cubesNumber /  (int)x, _cubesNumber / (int)y, _cubesNumber / (int)z) / _lodDownscale;
+
+            var cubesBuffer = CreateCubesBuffer(cubes, _cubesNumber);
+            var trianglesBuffer = new ComputeBuffer(totalCount * 5, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+            
+            _shader.SetInt("lod_downscale", _lodDownscale);
+            _shader.SetInt("cubes_number", _cubesNumber);
+            _shader.SetFloat("threshold", _threshold);
+            
+            _shader.SetBuffer(kernelIndex, "cubes", cubesBuffer);
+            _shader.SetBuffer(kernelIndex, "triangles", trianglesBuffer);
+            
+            _shader.Dispatch(kernelIndex, groupsCount.x, groupsCount.y, groupsCount.z);
+
+            // TODO : GET BUFFER TRIANGLES COUNT
+            var triangles = new Triangle[totalCount * 5];
+            trianglesBuffer.GetData(triangles, 0, 0, triangles.Length);
+            
+            cubesBuffer.Release();
+            trianglesBuffer.Release();
+            
+            _meshFilter.mesh = TrianglesToMesh(triangles);
+        }
+
+        private static ComputeBuffer CreateCubesBuffer(Vector4[,,] cubes, int cubesNumber)
+        {
+            var totalCount = cubesNumber * cubesNumber * cubesNumber;
+            var data = new Vector4[totalCount];
+
+            for (var x = 0; x < cubesNumber; x++)
+            {
+                for (var y = 0; y < cubesNumber; y++)
+                {
+                    for (var z = 0; z < cubesNumber; z++)
+                        data[x * cubesNumber * cubesNumber + y * cubesNumber + z] = cubes[x, y, z];
                 }
             }
 
+            var buffer = new ComputeBuffer(totalCount, sizeof(float) * 4);
+            buffer.SetData(data);
+            return buffer;
+        }
+
+        public void UpdateMeshCPU(Vector4[,,] cubes)
+        {
+            var skip = 1 << _lod;
+            var triangles = new List<Triangle>();
+
+            for (var x = 0; x < _cubesNumber - skip; x += skip)
+            {
+                for (var y = 0; y < _cubesNumber - skip; y += skip)
+                {
+                    for (var z = 0; z < _cubesNumber - skip; z += skip)
+                        triangles.AddRange(ProcessCube(cubes, x, y, z));
+                }
+            }
+            
+            _meshFilter.mesh = TrianglesToMesh(triangles.ToArray());
+        }
+
+        private Mesh TrianglesToMesh(Triangle[] triangles)
+        {
             var meshVerts = new List<Vector3>();
             var meshTris = new List<int>();
 
-            for (var triIndex = 0; triIndex < triangles.Count; triIndex++)
+            for (var triIndex = 0; triIndex < triangles.Length; triIndex++)
             {
                 var triangle = triangles[triIndex];
 
@@ -44,19 +149,19 @@ namespace PlanetGeneration
 
             return mesh;
         }
-        
-        private static List<Triangle> ProcessCube(Vector4[,,] cubes, float threshold, int skip, int x, int y, int z)
+
+        private List<Triangle> ProcessCube(Vector4[,,] cubes, int x, int y, int z)
         {
             var corners = new[]
             {
                 cubes[x, y, z],
-                cubes[x + skip, y, z],
-                cubes[x + skip, y, z + skip],
-                cubes[x, y, z + skip],
-                cubes[x, y + skip, z],
-                cubes[x + skip, y + skip, z],
-                cubes[x + skip, y + skip, z + skip],
-                cubes[x, y + skip, z + skip]
+                cubes[x + _lodDownscale, y, z],
+                cubes[x + _lodDownscale, y, z + _lodDownscale],
+                cubes[x, y, z + _lodDownscale],
+                cubes[x, y + _lodDownscale, z],
+                cubes[x + _lodDownscale, y + _lodDownscale, z],
+                cubes[x + _lodDownscale, y + _lodDownscale, z + _lodDownscale],
+                cubes[x, y + _lodDownscale, z + _lodDownscale]
             };
 
             var triangles = new List<Triangle>();
@@ -64,7 +169,7 @@ namespace PlanetGeneration
 
             for (var i = 0; i < corners.Length; i++)
             {
-                if (corners[i].w >= threshold)
+                if (corners[i].w >= _threshold)
                     cubeIndex |= 1 << i;
             }
 
@@ -73,24 +178,24 @@ namespace PlanetGeneration
                 if (MarchingCubesTables.TriangulationTable[cubeIndex, triIndex] == -1)
                     break;
 
-                var verts = new List<Vector3>();
+                var triangle = new Triangle();
 
                 for (var vertIndex = 0; vertIndex < 3; vertIndex++)
                 {
                     var v1 = corners[MarchingCubesTables.Corner1Index[MarchingCubesTables.TriangulationTable[cubeIndex, triIndex + vertIndex]]];
                     var v2 = corners[MarchingCubesTables.Corner2Index[MarchingCubesTables.TriangulationTable[cubeIndex, triIndex + vertIndex]]];
-                    verts.Add(GetMiddlePoint(v1, v2, threshold));
+                    triangle[vertIndex] = GetMiddlePoint(v1, v2);
                 }
 
-                triangles.Add(new Triangle(verts[0], verts[1], verts[2]));
+                triangles.Add(triangle);
             }
 
             return triangles;
         }
         
-        private static Vector3 GetMiddlePoint(Vector4 v1, Vector4 v2, float threshold)
+        private Vector3 GetMiddlePoint(Vector4 v1, Vector4 v2)
         {
-            return Vector3.Lerp(v1, v2, (threshold - v1.w) / (v2.w - v1.w));
+            return Vector3.Lerp(v1, v2, (_threshold - v1.w) / (v2.w - v1.w));
         }
     }
 }
